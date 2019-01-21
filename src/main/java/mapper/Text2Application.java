a64ef2dd334710ebb1186ebbab51abca18b228ea
wangguangyuan <wangguangyuan@DESKTOP-UTTEPSE>
2019-01-21 16:44:05 +0800

    初始项目3

---
+++ src/main/java/mapper/Text2Application.java
@@ -0,0 +1,14 @@
+package mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Text2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Text2Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/mapper/Text2ApplicationTests.java
@@ -0,0 +1,17 @@
+package mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Text2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
